d0d1fe7211b01b4c99578fb918301c5a2022634d
xinghaoru <867791585@qq.com>
2020-04-09 06:34:05 -0400

    add comments to problem #5

--- DCP.Solutions/ConcCarCdr/Solution.java
+++ DCP.Solutions/ConcCarCdr/Solution.java
@@ -30,21 +30,37 @@ public class Solution {
 **/
 	public Solution() {}
 	
+	// def cons(a, b)
 	public static<T, U, R> Function<BiFunction<T, U, R>, R> cons(T a, U b) {
+		
+		// def pair(f)
 		Function<BiFunction<T, U, R>, R> pair;
+		
+		// return f(a, b)
 		pair = f -> f.apply(a, b);
+		
+		// return pair
 		return pair;
 	}
 	
 	public static<T, U> T car(Function<BiFunction<T, U, T>, T> pair) {
+		
+		// return pair(return_first(a, b))
 		return pair.apply((a, b) -> a);
+		
+		// p.s., return first(a, b) = a
 	}
 	
 	public static<T, U> U cdr(Function<BiFunction<T, U, U>, U> pair) {
+		
+		// return pair(return_last(a, b))
 		return pair.apply((a, b) -> b);
+		
+		// p.s., return_last(a, b) = b
 	}
 
 	public static void main(String[] args) {
+		
 		Solution solution = new Solution();
 		System.out.println(solution.car(cons(2,3)));
 		System.out.println(solution.cdr(cons(2,3)));
