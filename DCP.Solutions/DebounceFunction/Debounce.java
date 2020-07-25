015e34595ba0d93574fb71684c8f738fcab86c1d
Haoru Xing <867791585@qq.com>
2020-07-25 01:12:16 -0400

    #105: Debounced Function (Unsolved)

---
+++ DCP.Solutions/DebounceFunction/Debounce.java
@@ -0,0 +1,24 @@
+package DebounceFunction;
+
+/*
+ * Debouncing in JavaScript is a practice used to improve browser performance.
+ * There might be some functionality in a web page which 
+ * requires time-consuming computations. 
+ * 
+ * If such a method is invoked frequently, it might greatly affect the performance 
+ * of the browser, as JavaScript is a single threaded language. 
+ * 
+ * Debouncing is a programming practice used to ensure that time-consuming tasks 
+ * do not fire so often, that it stalls the performance of the web page. 
+ * 
+ * In other words, it limits the rate at which a function gets invoked.
+ * **/
+
+public class Debounce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DCP.Solutions/DebounceFunction/ProblemDescription.md
@@ -0,0 +1,15 @@
+# Problem #105: Currency Arbitrage (Hard)
+
+## Asked By:
+
+This problem was asked by Facebook.
+
+## Description:
+ 
+Given a function f, and N return a debounced f of N milliseconds.  
+
+That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.
+
+## Example:
+
+## Note:
